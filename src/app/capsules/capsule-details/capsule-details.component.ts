c0cf007dbb7cc836128db2fe7f63714af3cb2615
Val Scrivner <vscrivner@affirma.com>
2019-11-12 21:11:41 -0800

    acccept more changes

--- src/app/capsules/capsule-details/capsule-details.component.ts
+++ src/app/capsules/capsule-details/capsule-details.component.ts
@@ -1,28 +1,15 @@
-<<<<<<< Updated upstream
-import { Component, OnInit } from '@angular/core';
-=======
 import { Component, Input } from '@angular/core';
 import { ICapsule } from '../capsule.model';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-capsule-details',
   templateUrl: './capsule-details.component.html',
   styleUrls: ['./capsule-details.component.scss']
 })
-<<<<<<< Updated upstream
-export class CapsuleDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-=======
 export class CapsuleDetailsComponent {
 
   @Input() selectedCapsule: ICapsule;
 
   constructor( ) { }
->>>>>>> Stashed changes
 
 }
--- src/app/capsules/capsule.model.ts
+++ src/app/capsules/capsule.model.ts
@@ -1,3 +1,17 @@
 export interface ICapsule {
   capsule_serial: string;
+  capsule_id: string;
+  status: string;
+  original_launch: Date | null;
+  original_launch_unix: number | null;
+  missions: ICapsuleMission[];
+  landings: number;
+  type: string;
+  details: string | null;
+  reuse_count: number;
+}
+
+export interface ICapsuleMission {
+  name: string;
+  flight: number;
 }
